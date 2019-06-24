b7a305d6ef16f9b27df9ac14f565ba0c7cdeb277
Chris <cjmarsh725@gmail.com>
2019-06-24 10:12:53 -0700

    CSS height tweaks

--- src/components/Journal/Journal.js
+++ src/components/Journal/Journal.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
 import './Journal.css';
 import values from 'lodash/values';
 import JournalEditor from './JournalEditor/JournalEditor';

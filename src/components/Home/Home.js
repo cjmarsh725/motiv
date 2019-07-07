fca882c8c7f0bc5d2b1bb66bcacc8de88d1ee843
Chris <cjmarsh725@gmail.com>
2019-07-07 09:47:27 -0700

    Images paired with styled text to provide content

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
+import { Image } from 'react-bootstrap';
 import './Home.css';
+
+const imgObjs = [
+  { 
+    url: "img/amelie-ohlrogge-BkmdKnuAZtw-unsplash.jpg",
+    text: "The only difference between an impasse and an opportunity is perspective"
+  },
+  {
+    url: "img/michael-louie-Zw5aEvl5QjA-unsplash.jpg",
+    text: "Remember all the work that has already gone into paving your way forward"
+  }
+]
 
 class Home extends Component {
   render() {
-    return <div>Home</div>;
+    return (
+      <div className="home-container">
+        {imgObjs.map(obj => { return (
+          <div>
+            <Image src={obj.url} fluid />
+            <div className="home-obj-text-container">
+              <div className="home-obj-text">{obj.text}</div>
+            </div>
+          </div>
+        )})}
+      </div>
+    );
   }
 }
 

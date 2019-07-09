05765cef4a34d6588503b9ff2bb12b3fdb277edb
Chris <cjmarsh725@gmail.com>
2019-07-09 11:15:25 -0700

    Multiple image file widths for faster loading

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,43 +4,43 @@ import './Home.css';
 
 const imgObjs = [
   { 
-    url: "img/amelie-ohlrogge-BkmdKnuAZtw-unsplash.jpg",
+    url: "img/cliff-",
     text: "The only difference between an impasse and an opportunity is perspective"
   },
   {
-    url: "img/michael-louie-Zw5aEvl5QjA-unsplash.jpg",
+    url: "img/road-",
     text: "Work has already gone into paving the way forward"
   },
   {
-    url: "img/cristina-gottardi-R4y_E5ZQDPg-unsplash.jpg",
+    url: "img/reflection-",
     text: "Reflection is all that is needed to see the world in a different light"
   },
   {
-    url: "img/robert-baker-IL526pUqQ1k-unsplash.jpg",
+    url: "img/layers-",
     text: "Life is always more complicated than it seems"
   },
   {
-    url: "img/erwan-hesry-RNLrB9MU--I-unsplash.jpg",
+    url: "img/grass-",
     text: "Knowledge inspires awe in even the smallest forms of life"
   },
   {
-    url: "img/ashley-knedler-OwpgxILRR7c-unsplash.jpg",
+    url: "img/erosion-",
     text: "The forces that wear us down also make us better"
   },
   {
-    url: "img/mauri-karlin-aUIbbwkPSkc-unsplash.jpg",
+    url: "img/spiral-",
     text: "Sometimes going in circles leads to progress"
   },
   {
-    url: "img/ron-manke-xghkLM1p1zA-unsplash.jpg",
+    url: "img/mountain-",
     text: "Every journey to the top of a mountain began with a single step"
   },
   {
-    url: "img/max-larochelle-tD-SDlQxfsY-unsplash.jpg",
+    url: "img/welder-",
     text: "The only secret to success is hard work"
   },
   {
-    url: "img/fabio-comparelli-uq2E2V4LhCY-unsplash.jpg",
+    url: "img/bridge-",
     text: "Perseverance while pursuing goals leads to fulfillment"
   },
 ]
@@ -49,15 +49,24 @@ class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      imgLoaded: new Array(imgObjs.length).fill(false)
+      imgLoaded: new Array(imgObjs.length).fill(false),
+      imgSize: this.getCurrentSize()
     }
-    this.shuffleArray(imgObjs);    
+    this.shuffleArray(imgObjs);
   }
 
   onImageLoad = index => {
     const { imgLoaded } = this.state;
     imgLoaded[index] = true;
     this.setState({ imgLoaded });
+  }
+
+  getCurrentSize = () => {
+    let currentSize;
+    if (window.innerWidth <= 800) currentSize = "small";
+    else if (window.innerWidth <= 1500) currentSize = "medium";
+    else currentSize = "large";
+    return currentSize;
   }
 
   shuffleArray = arr => {
@@ -75,7 +84,10 @@ class Home extends Component {
         {imgObjs.map((obj, i) => { return (
           <div key={i} className={"home-obj-container" +  
                 (this.state.imgLoaded[i] ? "" : " home-obj-toggle")}>
-            <Image src={obj.url} onLoad={() => this.onImageLoad(i)} fluid />
+            <Image 
+                src={obj.url + this.state.imgSize + ".jpg"}
+                onLoad={() => this.onImageLoad(i)}
+                fluid />
             <div className="home-obj-text-container">
               <div className={"home-obj-text" +  (this.state.imgLoaded[i] ? 
                               "" : " home-obj-toggle")}>

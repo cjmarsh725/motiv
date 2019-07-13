8554a4bd51c30c1645a493f3dfa766197ac2b147
Chris <cjmarsh725@gmail.com>
2019-07-13 16:29:38 -0700

    Calendar month picker ui

--- src/components/Schedule/ScheduleCalendar/ScheduleCalendar.js
+++ src/components/Schedule/ScheduleCalendar/ScheduleCalendar.js
@@ -1,34 +1,87 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ScheduleWeek from '../ScheduleWeek/ScheduleWeek';
 import './ScheduleCalendar.css';
 
-const ScheduleCalendar = props => {
-  const calendarWeeks = Array(5).fill("Schedule Week ");
-  const month = props.m.format('MMMM');
-  const m = props.m.clone().startOf('month').startOf('week');
-  return (
-    <div className="schedulecalendar-container">
-      <div className="schedulecalendar-month">
-        <div className="schedulecalendar-month-btn"
-              onClick={() => props.changeMonth(-1)}>
-          <i className="fas fa-caret-up"></i>
+class ScheduleCalendar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isPickerOpen: false
+    }
+  }
+
+  togglePicker = () => {
+    this.setState({ isPickerOpen: !this.state.isPickerOpen });
+  }
+
+  createMonthPicker = () => {
+    const months = [["January", "February", "March"], 
+                    ["April", "May", "June"],
+                    ["July", "August", "September"],
+                    ["October", "November", "December"]];
+    return (
+      months.map(quarter => { return (
+        <div className="schedulecalendar-picker-month-row">
+          {quarter.map(month => { return (
+            <div className="schedulecalendar-picker-month-item">
+              {month}
+            </div>);
+          })}
         </div>
-        <div className="schedulecalendar-month-label">
-          {month}
+      );})
+    )
+  }
+
+  render() {
+    const props = this.props;
+    const calendarWeeks = Array(5).fill("Schedule Week ");
+    const m = props.m.clone().startOf('month').startOf('week');
+    return (
+      <div className="schedulecalendar-container">
+        {/* Month Picker */}
+        <div className={this.state.isPickerOpen ? "" : 
+                          "schedulecalendar-picker-closed"}>
+          <div className="schedulecalendar-picker-content">
+            <div className="schedulecalendar-picker-year">
+              <div className="schedulecalendar-picker-year-btn">
+                <i className="fas fa-caret-left"></i>
+              </div>
+              {props.m.format('YYYY')}
+              <div className="schedulecalendar-picker-year-btn">
+                <i className="fas fa-caret-right"></i>
+              </div>
+            </div>
+            <div className="schedulecalendar-picker-month">
+              {this.createMonthPicker()}
+            </div>
+          </div>
+          <div className="schedulecalendar-picker-mask"
+              onClick={this.togglePicker}></div>
         </div>
-        <div className="schedulecalendar-month-btn"
-              onClick={() => props.changeMonth(1)}>
-          <i className="fas fa-caret-down"></i>
+        {/* Calendar */}
+        <div className="schedulecalendar-month">
+          <div className="schedulecalendar-month-btn"
+                onClick={() => props.changeMonth(-1)}>
+            <i className="fas fa-caret-up"></i>
+          </div>
+          <div className="schedulecalendar-month-label"
+              onClick={this.togglePicker}>
+            {props.m.format('MMMM YYYY')}
+          </div>
+          <div className="schedulecalendar-month-btn"
+                onClick={() => props.changeMonth(1)}>
+            <i className="fas fa-caret-down"></i>
+          </div>
         </div>
+        {calendarWeeks.map((x, i) => { return (
+          <ScheduleWeek 
+              key={x + i} 
+              m={m.clone().add(i, 'w')}
+              month={props.m.format('MMMM')} />
+        )})}
       </div>
-      {calendarWeeks.map((x, i) => { return (
-        <ScheduleWeek 
-            key={x + i} 
-            m={m.clone().add(i, 'w')}
-            month={month} />
-      )})}
-    </div>
-  );
+    );
+  }
 }
 
 export default ScheduleCalendar;

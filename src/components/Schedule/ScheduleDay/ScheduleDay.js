4d8b058fb9ad2dc5f109a7788ae984d18536bea7
Chris <cjmarsh725@gmail.com>
2019-07-23 14:47:42 -0700

    Time labeling tweaks

--- src/components/Schedule/ScheduleDay/ScheduleDay.js
+++ src/components/Schedule/ScheduleDay/ScheduleDay.js
@@ -9,13 +9,18 @@ class ScheduleDay extends Component {
     return (
       <div className="scheduleday-container">
         <div className="scheduleday-time-label">
+          <div className="scheduleday-time-label-half">AM</div>
           <div>{this.props.m.format("dddd, MMMM Do YYYY")}</div>
+          <div className="scheduleday-time-label-half">PM</div>
         </div>
         <div className="scheduleday-time">
           {timeBlocks.map((x, i) => {
             time.add(1, "hour");
             return (
-              <div key={x + i} className="scheduleday-time-block">{time.format("H:00")}</div>
+              <div key={x + i} className="scheduleday-time-block">
+                <div className="scheduleday-time-block-am">{i < 9 ? <span style={{paddingLeft: "6px"}}></span> : null}{time.format("H:00")}</div>
+                <div className="scheduleday-time-block-pm">{time.format("H:00")}</div>
+              </div>
           );})}
         </div>
       </div>

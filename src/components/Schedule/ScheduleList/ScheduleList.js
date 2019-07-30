970664ac75bd22dbebc0b7d1f6ba4779124e5a8e
Chris <cjmarsh725@gmail.com>
2019-07-30 12:54:45 -0700

    Display appointments only after selected date

--- src/components/Schedule/ScheduleList/ScheduleList.js
+++ src/components/Schedule/ScheduleList/ScheduleList.js
@@ -3,24 +3,36 @@ import moment from 'moment';
 import './ScheduleList.css';
 
 class ScheduleList extends Component {
+  getSubtitle = () => {
+    let subtitle = "Upcoming appointments:";
+    if (!this.props.mNow.isSame(this.props.mSelected, "day")) {
+      subtitle = "Appointments on or after " + this.props.mSelected.format("MMMM Do YYYY");
+    }
+    return subtitle;
+  }
+
   render() {
     return (
       <div className="schedulelist-container">
         <div className="schedulelist-header">
           {"Today is " + this.props.mNow.format("dddd, MMMM Do YYYY")}
-          <div className="schedulelist-header-subtitle">Upcoming appointments:</div>
+          <div className="schedulelist-header-subtitle">{this.getSubtitle()}</div>
         </div>
         <div className="schedulelist-list">
-          {this.props.schedule.map(item => { 
-            const itemTime = moment(item.date, "MM-DD-YYYY").format("dddd, MMMM Do YYYY") + " at " + item.startTime;
-            return (
-            <div key={itemTime} className="schedulelist-item">
-              <div className="schedulelist-item-title">{item.title}</div>
-              <div className="schedulelist-item-date">
-                {itemTime}
-              </div>
-            </div>
-          );})}
+          {this.props.schedule.map(item => {
+            const itemMoment = moment(item.date, "MM-DD-YYYY");
+            const itemTime = itemMoment.format("dddd, MMMM Do YYYY") + " at " + item.startTime;
+            if (itemMoment.isSameOrAfter(this.props.mSelected, "day")) {
+              return (
+                <div key={itemTime} className="schedulelist-item">
+                  <div className="schedulelist-item-title">{item.title}</div>
+                  <div className="schedulelist-item-date">
+                    {itemTime}
+                  </div>
+                </div>
+              );
+            } else return null;
+          })}
         </div>
       </div>
     );

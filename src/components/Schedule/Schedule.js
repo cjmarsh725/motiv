7cd29f108ce1d0571b8790ef973b77e0845db58a
Chris <cjmarsh725@gmail.com>
2019-08-06 10:13:09 -0700

    Updated schedule dates for testing purposes

--- src/components/Schedule/Schedule.js
+++ src/components/Schedule/Schedule.js
@@ -14,19 +14,19 @@ class Schedule extends Component {
       schedule: [
         {
           title: "Coding Preparation Meetup",
-          date: "07-29-2019",
+          date: "08-7-2019",
           startTime: "6:00 PM",
           endTime: "7:30 PM"
         },
         {
           title: "Bob's Birthday Party",
-          date: "07-30-2019",
+          date: "08-8-2019",
           startTime: "9:00 PM",
           endTime: "2:00 AM"
         },
         {
           title: "Dentist Appointment",
-          date: "07-31-2019",
+          date: "08-9-2019",
           startTime: "8:00 AM",
           endTime: "9:00 AM"
         },

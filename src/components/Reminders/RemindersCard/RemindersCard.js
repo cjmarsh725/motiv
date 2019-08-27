eb6279d65220999d3c6b0a56c2e3160641a488ab
Chris <cjmarsh725@gmail.com>
2019-08-27 10:09:43 -0700

    Close button styling

--- src/components/Reminders/RemindersCard/RemindersCard.js
+++ src/components/Reminders/RemindersCard/RemindersCard.js
@@ -4,6 +4,7 @@ import './RemindersCard.css';
 const RemindersCard = props => {
   return (
     <div className="reminderscard">
+      <div className="reminderscard-close-btn">x</div>
       <div className="reminderscard-content">{props.content}</div>
     </div>
   );

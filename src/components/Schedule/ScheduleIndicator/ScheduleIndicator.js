0f007bf35348167b5e8c9248faca3564e4487653
Chris <cjmarsh725@gmail.com>
2019-09-01 11:18:39 -0700

    Function for number of matching schedule dates

--- src/components/Schedule/ScheduleIndicator/ScheduleIndicator.js
+++ src/components/Schedule/ScheduleIndicator/ScheduleIndicator.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
+import moment from 'moment';
 import './ScheduleIndicator.css';
 
-const ScheduleIndicator = props => {
-  return (
-    <div className="scheduleindicator">O</div>
-  );
+class ScheduleIndicator extends Component {
+  getMatchingDates = () => {
+    let count = 0;
+    this.props.schedule.forEach(item => {
+      const date = moment(item.date, "MM-DD-YYYY h:mm A");
+      if (date.isSame(this.props.m, "day")) count++;
+    });
+    return count;
+  }
+
+  render() {
+    return (
+      <div className="scheduleindicator">
+        {this.getMatchingDates()}
+      </div>
+    );
+  }
 }
 
 export default ScheduleIndicator;

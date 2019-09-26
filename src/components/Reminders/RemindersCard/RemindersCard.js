232c76da789d07c20afb5040d4ce85af97241734
Chris <cjmarsh725@gmail.com>
2019-09-26 11:48:19 -0700

    Improved commenting

--- src/components/Reminders/RemindersCard/RemindersCard.js
+++ src/components/Reminders/RemindersCard/RemindersCard.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import './RemindersCard.css';
 
+/*
+This component is responsible for displaying reminders. The props passed down to it are a delete function,
+the content to display in the reminder, the index in the original array of reminders, a function to be
+called on drag, and a function to be called on drop.
+*/
 const RemindersCard = props => {
   return (
     <div className="reminderscard"

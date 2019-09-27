31deca966c773bca0433bd817528de936b58fec1
Chris <cjmarsh725@gmail.com>
2019-09-27 15:17:35 -0700

    Improved commenting

--- src/components/Schedule/ScheduleWeek/ScheduleWeek.js
+++ src/components/Schedule/ScheduleWeek/ScheduleWeek.js
@@ -2,6 +2,14 @@ import React from 'react';
 import ScheduleIndicator from '../ScheduleIndicator/ScheduleIndicator';
 import './ScheduleWeek.css';
 
+/*
+This component is responsible for displaying the row of days for each week in the calendar.
+The props passed down from the ScheduleCalendar component include the moment object from the
+calendar, the month, the current date, the selected date, the entire schedule, and a function
+to set which date is the selected date. Styling is adjusted for each day based on whether or
+not the day is in the current month, selected, or the current day. The ScheduleIndicator
+component is used to show how many appointments are on a given day.
+*/
 const ScheduleWeek = props => {
   const weekDays = Array(7).fill("Schedule Day ");
   return (

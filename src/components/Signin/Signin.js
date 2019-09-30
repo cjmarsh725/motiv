b89dc93b6dc423faeb3e5a9de9a4d727c33a0dfd
Chris <cjmarsh725@gmail.com>
2019-09-30 12:25:01 -0700

    Added input fields and relevant state

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -2,9 +2,34 @@ import React, { Component } from 'react';
 import './Signin.css';
 
 class Signin extends Component {
+  state = {
+    username: "",
+    password: ""
+  }
+
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
   render() {
     return (
-      <div>Signin</div>
+      <div className="signin">
+        <div className="signin-label-container">
+
+        </div>
+        <div className="signin-input-container">
+          <input className="signin-input-username"
+                    type="text"
+                    name="username"
+                    value={this.state.username}
+                    onChange={this.onChange} />
+          <input className="signin-input-password"
+                    type="password"
+                    name="password"
+                    value={this.state.password}
+                    onChange={this.onChange} />
+        </div>
+      </div>
     );
   }
 }

6a9f6aa01a6085580fcec3235b99fe653c3008dd
Chris <cjmarsh725@gmail.com>
2019-10-01 16:58:59 -0700

    Form layout and styling

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -14,21 +14,23 @@ class Signin extends Component {
   render() {
     return (
       <div className="signin">
-        <div className="signin-label-container">
-          <div>Username:</div>
-          <div>Password:</div>
-        </div>
-        <div className="signin-input-container">
-          <input className="signin-input-username"
-                    type="text"
-                    name="username"
-                    value={this.state.username}
-                    onChange={this.onChange} />
-          <input className="signin-input-password"
-                    type="password"
-                    name="password"
-                    value={this.state.password}
-                    onChange={this.onChange} />
+        <div className="signin-info-container">
+          <div className="signin-label-container">
+            <div>Username:</div>
+            <div>Password:</div>
+          </div>
+          <div className="signin-input-container">
+            <input className="signin-input-username"
+                      type="text"
+                      name="username"
+                      value={this.state.username}
+                      onChange={this.onChange} />
+            <input className="signin-input-password"
+                      type="password"
+                      name="password"
+                      value={this.state.password}
+                      onChange={this.onChange} />
+          </div>
         </div>
         <div className="signin-confirm-btn">
           Sign In

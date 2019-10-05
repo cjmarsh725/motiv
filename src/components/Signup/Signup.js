fe70f624261d64797bfd1e8e2ad82e08625a540d
Chris <cjmarsh725@gmail.com>
2019-10-05 12:30:57 -0700

    Signup initial framework

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -2,9 +2,40 @@ import React, { Component } from 'react';
 import './Signup.css';
 
 class Signup extends Component {
+  state = {
+    username: "",
+    password: ""
+  }
+
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
   render() {
     return (
-      <div>Signup</div>
+      <div className="signup">
+        <div className="signup-info-container">
+          <div className="signup-label-container">
+            <div>Username:</div>
+            <div>Password:</div>
+          </div>
+          <div className="signup-input-container">
+            <input className="signup-input"
+                      type="text"
+                      name="username"
+                      value={this.state.username}
+                      onChange={this.onChange} />
+            <input className="signup-input"
+                      type="password"
+                      name="password"
+                      value={this.state.password}
+                      onChange={this.onChange} />
+          </div>
+        </div>
+        <div className="signup-confirm-btn">
+          Sign Up
+        </div>
+      </div>
     );
   }
 }

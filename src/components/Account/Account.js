07bf47b8649ad4e315ef1879ca765541c1360cbd
Chris <cjmarsh725@gmail.com>
2019-10-06 11:08:32 -0700

    Removed dropdown and added basic account page

---
+++ src/components/Account/Account.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Account.css'
+
+class Account extends Component {
+  render() {
+    return (
+      <div className="account">
+        Account
+      </div>
+    );
+  }
+}
+
+export default Account;
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, NavLink, Route } from 'react-router-dom';
-import { Navbar, Nav, Dropdown } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
 import Home from '../Home/Home';
 import Journal from '../Journal/Journal';
 import Schedule from '../Schedule/Schedule';
 import Reminders from '../Reminders/Reminders';
+import Account from '../Account/Account';
 import Signin from '../Signin/Signin';
 import Signup from '../Signup/Signup';
 import './App.css';
@@ -24,18 +25,7 @@ function App() {
             <Nav.Link as={NavLink} to="/journal">Journal</Nav.Link>
             <Nav.Link as={NavLink} to="/schedule">Schedule</Nav.Link>
             <Nav.Link as={NavLink} to="/reminders">Reminders</Nav.Link>
-            <Dropdown>
-              <Dropdown.Toggle id="dropdown-custom-1">Pow! Zoom!</Dropdown.Toggle>
-              <Dropdown.Menu className="bg-dark">
-                <Dropdown.Item eventKey="1">Action</Dropdown.Item>
-                <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
-                <Dropdown.Item eventKey="3" active>
-                  Active Item
-                </Dropdown.Item>
-                <Dropdown.Divider />
-                <Dropdown.Item eventKey="4">Separated link</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
+            <Nav.Link as={NavLink} to="/account">Account</Nav.Link>
           </Nav>
         </Navbar.Collapse>
         <Navbar.Brand>
@@ -47,6 +37,7 @@ function App() {
         <Route path="/journal" component={Journal} />
         <Route path="/schedule" component={Schedule} />
         <Route path="/reminders" component={Reminders} />
+        <Route path="/account" component={Account} />
         <Route path="/signin" component={Signin} />
         <Route path="/signup" component={Signup} />
       </div>

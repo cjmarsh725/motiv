9efcdb979287a407a34e323d7a0017382f6c3f6b
Chris <cjmarsh725@gmail.com>
2019-10-09 10:52:02 -0700

    Initial jsx for labels and buttons

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -2,10 +2,36 @@ import React, { Component } from 'react';
 import './Account.css'
 
 class Account extends Component {
+  getLabels = signedIn => {
+    if (signedIn) return ( <>
+      <div>Sign out of your account:</div>
+      <div>Permanently delete your account:</div>
+    </>); else return ( <>
+      <div>Sign in to an existing account:</div>
+      <div>Sign up for a new account:</div>
+    </>);
+  }
+
+  getButtons = signedIn => {
+    if (signedIn) return ( <>
+      <div className="account-btn">Sign Out</div>
+      <div className="account-btn">Delete</div>
+    </>); else return ( <>
+      <div className="account-btn">Sign In</div>
+      <div className="account-btn">Sign Up</div>
+    </>);
+  }
+
   render() {
+    const signedIn = true;
     return (
       <div className="account">
-        Account
+        <div className="account-labels">
+          {this.getLabels(signedIn)}
+        </div>
+        <div className="account-buttons">
+          {this.getButtons(signedIn)}
+        </div>
       </div>
     );
   }

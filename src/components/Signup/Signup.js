2b4dcf268157053386b61db7be5032741b3ad89d
Chris <cjmarsh725@gmail.com>
2019-11-05 14:40:44 -0800

    Improved commenting

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import './Signup.css';
 
+/*
+The Signup component contains the form used to sign up for a new account. There is a link
+to the Signin page and input fields for the username and password of an account. Entering
+a users details and clicking the button will send a request to the server to create a new
+account with the specified credentials and return a token that is then saved to local
+storage for use in the other endpoints.
+*/
 class Signup extends Component {
   state = {
     username: "",
@@ -9,29 +16,37 @@ class Signup extends Component {
     error: false
   }
 
+  // Helper function to redirect a user to the signin route
   redirect = () => {
     this.props.history.push('/signin');
   }
 
+  // Updates the username or password and resets the error field to false
   onChange = e => {
     this.setState({ [e.target.name]: e.target.value, error: false });
   }
 
+  // When the form is submitted a request is made to the backend to create a new user
   onSubmit = () => {
     const { username, password } = this.state;
+    // Makes a request to the backend for a new user and the authorization token
     axios
     .post(process.env.REACT_APP_BACKEND + '/users/signup', { username, password })
     .then(response => {
+      // Stores the token in local storage for use in other routes
       localStorage.setItem('token', response.data.token);
+      // Redirect to the home page
       this.props.history.push('/');
     })
     .catch(err => {
       console.log(err);
+      // Activates the error message and removes any token in local storage
       this.setState({ error: true });
       localStorage.removeItem('token');
     });
   }
 
+  // Helper function to submit the form when enter is pressed in the final field
   handleKeyPress = e => {
     if (e.key === 'Enter') {
       this.onSubmit();
